20f096bc02e6dba82b8fd7090858af05a0ebcabb
EC2 Default User <ec2-user@ip-172-31-63-79.ec2.internal>
2018-09-17 12:56:07 +0000

    add export Instance ID for clean up.

--- Week 1/Exercise 3/deployment.sh
+++ Week 1/Exercise 3/deployment.sh
@@ -34,3 +34,4 @@ pip install awscli --upgrade --user
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 sudo yum install -y session-manager-plugin.rpm
 rm session-manager-plugin.rpm
+export InstanceId=$InstanceId
