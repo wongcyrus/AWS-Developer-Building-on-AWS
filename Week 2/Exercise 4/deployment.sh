d83655e5283e0bc5aa85123f28ac5604f9b6774b
EC2 Default User <ec2-user@ip-172-31-95-222.ec2.internal>
2018-09-18 10:52:16 +0000

    Fix export logic error, as it does not work after set the env variable.

--- Week 2/Exercise 4/deployment.sh
+++ Week 2/Exercise 4/deployment.sh
@@ -3,11 +3,14 @@ SourceBucket=auniquesourcebucketname
 cp ../../Week\ 1/Exercise\ 3/vpc.template .
 aws s3api create-bucket --bucket $SourceBucket
 aws s3 cp vpc.template s3://$SourceBucket
+rm vpc.template
 aws cloudformation create-stack --stack-name edx-project-stack --template-body file://cfn.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameters ParameterKey=Password,ParameterValue=P@ssword ParameterKey=SourceBucket,ParameterValue=$SourceBucket 
 aws cloudformation wait stack-create-complete --stack-name edx-project-stack
-export AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
+AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
 --query 'Stacks[0].Outputs[?OutputKey==`AccessKey`].OutputValue' --output text)
-export AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
+AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
 --query 'Stacks[0].Outputs[?OutputKey==`SecretKey`].OutputValue' --output text)
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
