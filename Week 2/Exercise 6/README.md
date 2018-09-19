96ca2e75736ac0e2ebe1b7286f7b923ffa2d5e76
EC2 Default User <ec2-user@ip-172-31-22-128.ec2.internal>
2018-09-19 02:47:33 +0000

    Exercise 7 partial works.

---
+++ Week 2/Exercise 6/README.md
@@ -0,0 +1,3 @@
+Repeat Week 2 Exercise 5 for the setup.
+
+
---
+++ Week 3/Exercise 7/deployment.sh
@@ -0,0 +1,24 @@
+export AWS_DEFAULT_REGION=us-east-1
+AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
+SourceBucket=sourcebucket$AWSAccountId
+cp ../../Week\ 1/Exercise\ 3/vpc.yaml .
+cp ../../Week\ 2/Exercise\ 5/iam.yaml .
+aws s3api create-bucket --bucket $SourceBucket
+sleep 5
+aws s3 cp iam.yaml s3://$SourceBucket
+aws s3 cp vpc.yaml s3://$SourceBucket
+aws s3 cp db.yaml s3://$SourceBucket
+rm vpc.yaml
+rm iam.yaml
+aws cloudformation create-stack --stack-name edx-project-stack --template-body file://cfn.yaml \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameters ParameterKey=Password,ParameterValue=P@ssword ParameterKey=SourceBucket,ParameterValue=$SourceBucket 
+aws cloudformation wait stack-create-complete --stack-name edx-project-stack
+AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
+--query 'Stacks[0].Outputs[?OutputKey==`AccessKey`].OutputValue' --output text)
+AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
+--query 'Stacks[0].Outputs[?OutputKey==`SecretKey`].OutputValue' --output text)
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+
+
