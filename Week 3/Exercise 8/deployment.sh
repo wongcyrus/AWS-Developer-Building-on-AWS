53f9563b6c841e0d4645b462e4b6bc8f052006de
EC2 Default User <ec2-user@ip-172-31-10-169.ec2.internal>
2018-09-19 09:34:38 +0000

    Web stack workable and partial work for user data.

--- Week 3/Exercise 8/deployment.sh
+++ Week 3/Exercise 8/deployment.sh
@@ -1,10 +1,20 @@
 export AWS_DEFAULT_REGION=us-east-1
 AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
 SourceBucket=sourcebucket$AWSAccountId
-cp ../../Week\ 1/Exercise\ 3/vpc.yaml .
-cp ../../Week\ 2/Exercise\ 5/iam.yaml .
+
 aws s3api create-bucket --bucket $SourceBucket
 sleep 5
+
+wget https://s3-us-west-2.amazonaws.com/us-west-2-tcdev/courses/AWS-100-ADG/v1.1.0/exercises/ex-rds.zip
+unzip ex-rds.zip
+cp app.ini exercise-rds/Deploy/
+cd exercise-rds
+zip -r deploy-app.zip Deploy/ FlaskApp/
+aws s3 cp deploy-app.zip s3://$SourceBucket/
+
+cp ../../Week\ 1/Exercise\ 3/vpc.yaml .
+cp ../../Week\ 2/Exercise\ 5/iam.yaml .
+
 aws s3 sync . s3://$SourceBucket 
 rm vpc.yaml
 rm iam.yaml
