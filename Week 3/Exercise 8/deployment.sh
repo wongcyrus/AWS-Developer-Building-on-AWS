100d95f966f71e89ac8b2db43832474e600b8e0b
EC2 Default User <ec2-user@ip-172-31-10-169.ec2.internal>
2018-09-19 11:44:19 +0000

    Refactor out parameters.yaml

--- Week 3/Exercise 8/deployment.sh
+++ Week 3/Exercise 8/deployment.sh
@@ -6,15 +6,17 @@ aws s3api create-bucket --bucket $SourceBucket
 sleep 5
 
 wget https://s3-us-west-2.amazonaws.com/us-west-2-tcdev/courses/AWS-100-ADG/v1.1.0/exercises/ex-rds.zip
-unzip ex-rds.zip
+unzip -o ex-rds.zip
+rm ex-rds.zip
 cp app.ini exercise-rds/Deploy/
 cd exercise-rds
-zip -r deploy-app.zip Deploy/ FlaskApp/
+zip -ro deploy-app.zip Deploy/ FlaskApp/
 aws s3 cp deploy-app.zip s3://$SourceBucket/
+cd ..
+rm -rf exercise-rds
 
 cp ../../Week\ 1/Exercise\ 3/vpc.yaml .
 cp ../../Week\ 2/Exercise\ 5/iam.yaml .
-
 aws s3 sync . s3://$SourceBucket 
 rm vpc.yaml
 rm iam.yaml
@@ -23,6 +25,13 @@ aws cloudformation create-stack --stack-name edx-project-stack --template-body f
 --parameters    ParameterKey=Password,ParameterValue=P@ssword \
                 ParameterKey=DBPassword,ParameterValue=Password \
                 ParameterKey=SourceBucket,ParameterValue=$SourceBucket
+
+# aws cloudformation update-stack --stack-name edx-project-stack \
+# --template-url ttps://s3.amazonaws.com/$SourceBucket/cfn.yaml \
+# --capabilities CAPABILITY_NAMED_IAM \
+# --parameters    ParameterKey=Password,ParameterValue=P@ssword \
+#                 ParameterKey=DBPassword,ParameterValue=Password \
+#                 ParameterKey=SourceBucket,ParameterValue=$SourceBucket
                 
 aws cloudformation wait stack-create-complete --stack-name edx-project-stack
 AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
