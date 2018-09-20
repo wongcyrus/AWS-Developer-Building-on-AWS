773e3cc7cb9de2e23aa930b62c5bb5bb1862ab44
EC2 Default User <ec2-user@ip-172-31-78-233.ec2.internal>
2018-09-20 01:45:38 +0000

    Add NAT2 to VPCm and fix partial userdata.

--- Week 3/Exercise 8/deployment.sh
+++ Week 3/Exercise 8/deployment.sh
@@ -8,7 +8,8 @@ sleep 5
 wget https://s3-us-west-2.amazonaws.com/us-west-2-tcdev/courses/AWS-100-ADG/v1.1.0/exercises/ex-rds.zip
 unzip -o ex-rds.zip
 rm ex-rds.zip
-cp app.ini exercise-rds/Deploy/
+yes | cp -f code/app.ini exercise-rds/Deploy/
+yes | cp -f code/config.py exercise-rds/FlaskApp/
 cd exercise-rds
 zip -ro deploy-app.zip Deploy/ FlaskApp/
 aws s3 cp deploy-app.zip s3://$SourceBucket/
@@ -20,18 +21,13 @@ cp ../../Week\ 2/Exercise\ 5/iam.yaml .
 aws s3 sync . s3://$SourceBucket 
 rm vpc.yaml
 rm iam.yaml
-aws cloudformation create-stack --stack-name edx-project-stack --template-body file://cfn.yaml \
+# aws cloudformation create-stack --stack-name edx-project-stack --template-body file://cfn.yaml \
+aws cloudformation update-stack --stack-name edx-project-stack \
+--template-url https://s3.amazonaws.com/$SourceBucket/cfn.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameters    ParameterKey=Password,ParameterValue=P@ssword \
                 ParameterKey=DBPassword,ParameterValue=Password \
                 ParameterKey=SourceBucket,ParameterValue=$SourceBucket
-
-# aws cloudformation update-stack --stack-name edx-project-stack \
-# --template-url ttps://s3.amazonaws.com/$SourceBucket/cfn.yaml \
-# --capabilities CAPABILITY_NAMED_IAM \
-# --parameters    ParameterKey=Password,ParameterValue=P@ssword \
-#                 ParameterKey=DBPassword,ParameterValue=Password \
-#                 ParameterKey=SourceBucket,ParameterValue=$SourceBucket
                 
 aws cloudformation wait stack-create-complete --stack-name edx-project-stack
 AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
@@ -41,4 +37,7 @@ AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name edx-proj
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
-
+pip install awscli --upgrade --user
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
+sudo yum install -y session-manager-plugin.rpm
+rm session-manager-plugin.rpm
