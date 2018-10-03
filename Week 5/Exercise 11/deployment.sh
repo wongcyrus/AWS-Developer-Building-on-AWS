97c759e07ae610ef7d9ee345f0ce15339e628954
EC2 Default User <ec2-user@ip-172-31-50-145.ec2.internal>
2018-10-03 00:51:18 +0000

    Completed Lambda deployments

--- Week 5/Exercise 11/deployment.sh
+++ Week 5/Exercise 11/deployment.sh
@@ -2,6 +2,9 @@ export AWS_DEFAULT_REGION=us-east-1
 AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
 SourceBucket=sourcebucket$AWSAccountId
 
+aws s3api create-bucket --bucket $SourceBucket
+sleep 5
+
 cp ../../Week\ 1/Exercise\ 3/vpc.yaml .
 cp ../../Week\ 2/Exercise\ 5/iam.yaml .
 cp ../../Week\ 4/Exercise\ 9/cdn.yaml .
@@ -30,16 +33,28 @@ yes | cp -f ../../Week\ 4/Polly/code/app.ini exercise-lambda/Deploy/
 cd exercise-lambda
 zip -ro deploy-app.zip Deploy/ FlaskApp/
 aws s3 cp deploy-app.zip s3://$SourceBucket/
-cd ..
+pip-3.6 install 'mysql_connector_python<8.1' -t LambdaImageLabels
+cd LambdaImageLabels
+zip -r lambda.zip *
+aws s3 cp lambda.zip s3://$SourceBucket/
+cd ../../
 rm -rf exercise-lambda
 
-aws cloudformation update-stack --stack-name edx-project-stack \
---template-url https://s3.amazonaws.com/$SourceBucket/cfn.yaml \
+random=$(shuf -i 2000-65000 -n 1)
+aws cloudformation create-stack --stack-name edx-project-stack --template-body file://cfn.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
---parameters    ParameterKey=Password,UsePreviousValue=true \
-                ParameterKey=DBPassword,UsePreviousValue=true \
-                ParameterKey=SourceBucket,UsePreviousValue=true \
-                ParameterKey=AppDomain,UsePreviousValue=true
+--parameters    ParameterKey=Password,ParameterValue=P@ssword \
+                ParameterKey=DBPassword,ParameterValue=Password \
+                ParameterKey=SourceBucket,ParameterValue=$SourceBucket \
+                ParameterKey=AppDomain,ParameterValue=uniqueedx$AWSAccountId$random
+# aws cloudformation wait stack-create-complete --stack-name edx-project-stack
+# aws cloudformation update-stack --stack-name edx-project-stack \
+# --template-url https://s3.amazonaws.com/$SourceBucket/cfn.yaml \
+# --capabilities CAPABILITY_NAMED_IAM \
+# --parameters    ParameterKey=Password,UsePreviousValue=true \
+#                 ParameterKey=DBPassword,UsePreviousValue=true \
+#                 ParameterKey=SourceBucket,UsePreviousValue=true \
+#                 ParameterKey=AppDomain,UsePreviousValue=true
                 
 aws cloudformation wait stack-update-complete --stack-name edx-project-stack
 
